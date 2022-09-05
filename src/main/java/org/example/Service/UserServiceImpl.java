70fa40c77e2fa91ae9833bc69b8903afb82ecff7
Max-None <106478561+Max-None@users.noreply.github.com>
2022-09-05 12:45:15 +0300

    EntityManager version 3.0 Interface UserService was added. Transaction management was transferred from UserDAO to UserService.

--- src/main/java/org/example/Service/UserServiceImpl.java
+++ src/main/java/org/example/Service/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService{
     UserDAO userDAO;
 
@@ -18,26 +17,31 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> index() {
         return userDAO.index();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(int id) {
         return userDAO.getUserById(id);
     }
 
     @Override
+    @Transactional
     public void save(User user) {
         userDAO.save(user);
     }
 
     @Override
+    @Transactional
     public void update(int id, User modifiedUser) {
         userDAO.update(id, modifiedUser);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
         userDAO.delete(id);
     }
